5f004f46bc428493fc59d5a5b1fa2683af9a0183
KhetrabasiReddy <reddykhetrabasi@gmail.com>
2025-01-29 17:34:38 +0530

    added about section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import NavBar from './components/NavBar';
 import { useEffect, useState } from 'react';
 import Loader from './components/Loader';
-import MouseTracker from './components/MouseTracker';
+// import MouseTracker from './components/MouseTracker';
 
 // style imports
 import './App.css'
@@ -10,8 +10,7 @@ import './styles/fontSize.css'
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import About from './components/About';
-import { ReactTyped } from "react-typed"
+import ContactMe from './components/ContactMe';
 
 
 function App() {
@@ -25,15 +24,13 @@ function App() {
 
   return (
     <>
-      {theme === 'dark' ? <MouseTracker /> : null}
+      {/* {theme === 'dark' ? <MouseTracker /> : null} */}
       {loading ? <Loader /> : <>
         <NavBar theme={theme} setTheme={setTheme} />
         <Home />
         <Projects />
         <Skills />
-        <About />
-       <h1 class="display-1 px-5 my-5"> <span class="text-danger fw-bold">NOTE:</span>This portfolio is currently a work in progress. New sections and content will be added soon<ReactTyped strings={["..."]} typeSpeed={80} backSpeed={30} loop /></h1>
-
+        <ContactMe />
       </>}
 
     </>
